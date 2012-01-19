0a7d0d9c70a56cdaaa2f2c1a525e6ffb789655ed
Colin Svingen <swoogan@gmail.com>
2012-01-18 22:42:38 -0600

    Implemeting the land and struture types

--- bestbuys/structure.go
+++ bestbuys/structure.go
@@ -1,14 +1,24 @@
 package main
 
+import "math"
+
+type Land struct {
+  name string
+  cost float64
+  income float64
+  retainAlways bool
+}
+
+
 type Structure struct {
   name string
   increase float64
-  builtOn Land
+  builtOn *Land
   income float64
   cost float64
 }
 
-func NewStructure(name string, cost, increase, income float64, land Land) (*Structure) {
+func NewStructure(name string, cost, increase, income float64, land *Land) (*Structure) {
   return &Structure { 
     name: name, 
     cost: cost, 
@@ -16,4 +26,27 @@ func NewStructure(name string, cost, increase, income float64, land Land) (*Stru
     income: income, 
     builtOn: land,
   }
+}
+
+func (s *Structure) IncreasePrice(quantity float64) {
+  s.cost += s.increase * quantity
+}
+
+func (s *Structure) timeToPurchase(quantity float64, income, cost float64) (float64) {
+  hoursToBase := float64(0)
+  
+  if !s.builtOn.retainAlways {
+    hoursToBase = (s.builtOn.cost * quantity) / income
+    income += s.builtOn.income * quantity
+  }
+
+  var hoursToLand = (s.cost * quantity) / income
+  var total = hoursToBase + hoursToLand
+  var adjustedTotal = math.Ceil(hoursToBase) + math.Ceil(hoursToLand)
+
+  if total <= 1 {
+    return 1
+  }
+
+  return adjustedTotal
 }
