1f205292a840dfca1c15be6a7ad7f5911ddf306e
Colin Svingen <Colin Svingen@localhost>
2012-01-19 20:49:27 -0600

    created new types, started mongo

--- bestbuys/structure.go
+++ bestbuys/structure.go
@@ -1,48 +1,52 @@
 package main
 
-import "math"
+import (
+  "math"
+  "launchpad.net/gobson/bson"
+)
 
 type Land struct {
-  name string
-  cost float64
-  income float64
-  retainAlways bool
+  game bson.ObjectId
+  Name string
+  Cost money
+  Income money
+  RetainAlways bool
 }
 
 
 type Structure struct {
   name string
-  increase float64
+  increase money
   builtOn *Land
-  income float64
-  cost float64
+  income money
+  cost money
 }
 
-func NewStructure(name string, cost, increase, income float64, land *Land) (*Structure) {
-  return &Structure { 
-    name: name, 
-    cost: cost, 
-    increase: increase, 
-    income: income, 
+func NewStructure(name string, cost, increase, income money, land *Land) (*Structure) {
+  return &Structure {
+    name: name,
+    cost: cost,
+    increase: increase,
+    income: income,
     builtOn: land,
   }
 }
 
-func (s *Structure) IncreasePrice(quantity float64) {
+func (s *Structure) IncreasePrice(quantity money) {
   s.cost += s.increase * quantity
 }
 
-func (s *Structure) timeToPurchase(quantity float64, income, cost float64) (float64) {
-  hoursToBase := float64(0)
-  
-  if !s.builtOn.retainAlways {
-    hoursToBase = (s.builtOn.cost * quantity) / income
-    income += s.builtOn.income * quantity
+func (s *Structure) timeToPurchase(quantity money, income, cost money) (money) {
+  hoursToBase := money(0)
+
+  if !s.builtOn.RetainAlways {
+    hoursToBase = (s.builtOn.Cost * quantity) / income
+    income += s.builtOn.Income * quantity
   }
 
   var hoursToLand = (s.cost * quantity) / income
   var total = hoursToBase + hoursToLand
-  var adjustedTotal = math.Ceil(hoursToBase) + math.Ceil(hoursToLand)
+  var adjustedTotal = money(math.Ceil(float64(hoursToBase)) + math.Ceil(float64(hoursToLand)))
 
   if total <= 1 {
     return 1
