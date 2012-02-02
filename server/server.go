a5f484598e90d45cccf190403607e45abe04b350
Colin Svingen <1377725+Swoogan@users.noreply.github.com>
2012-02-01 23:32:30 -0600

    server/server.go

---
+++ server/server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"http"
+	"log"
+	"launchpad.net/mgo"
+	"bitbucket.org/Swoogan/mongorest"
+)
+
+func main() {
+	log.Printf("Connecting to mongodb")
+
+	session, err := mgo.Mongo("172.16.1.63")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer session.Close()
+
+	db := session.DB("uken")
+
+	mongorest.NewMongoRest(db, "finances")
+	mongorest.NewMongoRest(db, "games")
+
+	log.Printf("About to listen on 4040")
+	err = http.ListenAndServe(":4040", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
