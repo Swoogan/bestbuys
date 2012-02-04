7a356ae13df2f433923bce0482bfffbec6a7d4c8
Colin Svingen <1377725+Swoogan@users.noreply.github.com>
2012-02-03 18:36:06 -0600

    partially implemented the command server

---
+++ command/server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"http"
+	"log"
+	"launchpad.net/mgo"
+	"github.com/Swoogan/rest.go"
+)
+
+func main() {
+	log.Printf("Connecting to mongodb")
+
+	session, err := mgo.Mongo("localhost")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer session.Close()
+
+	db := session.DB("command")
+
+	tr := NewTaskRest(db.C("tasks"))
+	rest.Resource("tasks", tr)
+
+	log.Printf("About to listen on 4041")
+	err = http.ListenAndServe(":4041", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
