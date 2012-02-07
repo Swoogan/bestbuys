0358720f2bfa05df5d91a8728b9385ffebf9c1eb
Colin Svingen <Colin Svingen@localhost>
2012-02-07 09:05:19 -0600

    Each field now submits the correct task. Added todo file

--- site/index.js
+++ site/index.js
@@ -2,8 +2,22 @@ $(document).ready(function() {
   var bindBehaviors = function() {
     $(".data").click(function() { this.contentEditable='true'; });
 
-    $(".data").focusout(function() {
-      alert($(this).html());
+    $(".income").focusout(function() {
+      $.post( "/tasks/", 
+              '{ "name": "setIncome", "data": {"income": "' + $(this).html() + '"} }',
+              {contentType: 'application/json'});
+    });
+    $(".upkeep").focusout(function() {
+      $.post( "/tasks/", 
+              '{ "name": "setUpkeep", "data": {"upkeep": "' + $(this).html() + '"} }',
+              {contentType: 'application/json'});
+    });
+    $(".balance").focusout(function() {
+      $.post( "/tasks/", 
+              '{ "name": "setBalance", "data": {"balance": "' + $(this).html() + '"} }',
+              {contentType: 'application/json'});
+    });
+    $(".wallet").focusout(function() {
       $.post( "/tasks/", 
               '{ "name": "setWallet", "data": {"wallet": "' + $(this).html() + '"} }',
               {contentType: 'application/json'});
