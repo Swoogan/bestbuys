beb035ce49bf52d8b301615fbb2c30c2c2e62913
Colin Svingen <1377725+Swoogan@users.noreply.github.com>
2012-02-07 11:52:14 -0600

    Fixed up the css so the text doesn't go beyond the div and deleting all the text doesn't shrink the div

--- site/format.js
+++ site/format.js
@@ -1,13 +1,24 @@
 function CurrencyFormatted(amount)
 {
   var f = parseFloat(amount);
-  if(isNaN(f)) { return '$0'; }
+  if(isNaN(f)) { return "$0"; }
+
   var s = new String(f);
   var temps = "";
   for (var i = s.length; i > 0; i -= 3)
   {
-     temps = s.slice(Math.max(i-3, 0), i) + ',' + temps
+     temps = s.slice(Math.max(i-3, 0), i) + "," + temps
   }
 
-  return '$' + temps.slice(0, -1);
+  return "$" + temps.slice(0, -1);
+}
+
+function ParseCurrency(amount)
+{
+  if (amount.length === "") { return 0; }
+  if (amount.charAt(0) == "$") { amount = amount.slice(1); }  
+  amount = amount.replace(",", "");
+  var i = parseInt(amount);
+  if(isNaN(f)) { return "0"; }
+  return i;
 }
