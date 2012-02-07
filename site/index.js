32f0c18c8bd500126449cd2fce57d10a240d6f9e
Colin Svingen <1377725+Swoogan@users.noreply.github.com>
2012-02-07 13:08:01 -0600

    Lost focus now removes the dropshadow on the fields

--- site/index.js
+++ site/index.js
@@ -10,21 +10,33 @@ $(document).ready(function() {
       $.post( "/tasks/", 
               '{ "name": "setIncome", "data": {"income": "' + $(this).html() + '"} }',
               {contentType: 'application/json'});
+      if (this.className.indexOf("shadow") != -1) {
+        this.className = this.className.replace(" shadow", "");
+      }
     });
     $(".upkeep").focusout(function() {
       $.post( "/tasks/", 
               '{ "name": "setUpkeep", "data": {"upkeep": "' + $(this).html() + '"} }',
               {contentType: 'application/json'});
+      if (this.className.indexOf("shadow") != -1) {
+        this.className = this.className.replace(" shadow", "");
+      }
     });
     $(".balance").focusout(function() {
       $.post( "/tasks/", 
               '{ "name": "setBalance", "data": {"balance": "' + $(this).html() + '"} }',
               {contentType: 'application/json'});
+      if (this.className.indexOf("shadow") != -1) {
+        this.className = this.className.replace(" shadow", "");
+      }
     });
     $(".wallet").focusout(function() {
       $.post( "/tasks/", 
               '{ "name": "setWallet", "data": {"wallet": "' + $(this).html() + '"} }',
               {contentType: 'application/json'});
+      if (this.className.indexOf("shadow") != -1) {
+        this.className = this.className.replace(" shadow", "");
+      }
     });
 
     $(".data").keypress(function(e){ return e.which != 13; });
