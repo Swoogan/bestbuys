02cf918648be792875aa43f56ebce2cc58eb2202
Colin Svingen <swoogan@gmail.com>
2012-02-08 22:45:38 -0600

    Adding the rpc calls from command->denormalizer

--- command/dispatcher.go
+++ command/dispatcher.go
@@ -2,29 +2,59 @@ package main
 
 import (
 	"log"
+	"rpc"
+	"denormalizer"
 )
 
-type Data map[string]interface{}
+type handler func(d denormalizer.Data)
+type handlerPool map[string]handler
 
-type Command struct {
+type command struct {
 	name string
-	data Data
+	data denormalizer.Data
 }
-
-type handlerPool map[string]func (cmd Command)
 
 type dispatcher struct {
 	pool handlerPool
 }
 
 func newDispatcher() dispatcher {
-	return dispatcher{ make(handlerPool)  }
+	pool := handlerPool {
+			"setWallet": setWallet,
+		}
+	return dispatcher{ pool }
 }
 
-func(d dispatcher) Dispatch(cmd Command) {
-	log.Println("Handle command")
+func(d dispatcher) Dispatch(cmd command) {
+	if handler, ok := d.pool[cmd.name]; ok {
+		handler(cmd.data)
+	} else {
+		log.Printf("No handler specified for command: %v", cmd.name)
+	}
 }
 
 type Dispatches interface {
-	Dispatch(cmd Command)
+	Dispatch(cmd command)
+}
+
+//
+// HANDLERS
+//
+
+func setWallet(d denormalizer.Data) {
+	client, err := rpc.DialHTTP("tcp", ":4042")
+	if err != nil {
+		log.Println("OMG connect failed need to queue this!1!!:", err)
+		return
+	}
+
+	event := &denormalizer.Event{"walletWasSet", d}
+	var reply int
+
+	err = client.Call("Denormalizer.HandleEvent", event, &reply)
+	if err != nil {
+		log.Println("OMG call failed, need to queue this!!!")
+	}
+
+	log.Printf("Handle Command: %v", "setWallet")
 }
