b1fe4375f567e232750a8451463571417ed51684
Colin Svingen <1377725+Swoogan@users.noreply.github.com>
2012-02-09 14:14:07 -0600

    Connected the denormalizer to mongodb

--- command/dispatcher.go
+++ command/dispatcher.go
@@ -21,6 +21,9 @@ type dispatcher struct {
 func newDispatcher() dispatcher {
 	pool := handlerPool {
 			"setWallet": setWallet,
+			"setUpkeep": setUpkeep,
+			"setBalance": setBalance,
+			"setIncome": setIncome,
 		}
 	return dispatcher{ pool }
 }
@@ -37,24 +40,56 @@ type Dispatches interface {
 	Dispatch(cmd command)
 }
 
-//
-// HANDLERS
-//
-
-func setWallet(d event.Data) {
-	client, err := rpc.DialHTTP("tcp", ":4042")
+func rpcCall(address string, method string, e *event.Event) {
+	client, err := rpc.DialHTTP("tcp", address)
 	if err != nil {
 		log.Println("OMG connect failed need to queue this!1!!:", err)
 		return
 	}
 
-	event := &event.Event{"walletWasSet", d}
 	var reply int
-
-	err = client.Call("Denormalizer.HandleEvent", event, &reply)
+	err = client.Call(method, e, &reply)
 	if err != nil {
 		log.Println("OMG call failed, need to queue this!!!")
 	}
+	client.Close()
+}
 
-	log.Printf("Handle Command: %v", "setWallet")
+func denormalize(e *event.Event) {
+	rpcCall(":4042", "Denormalizer.HandleEvent", e)
+}
+
+func schedule(e *event.Event) {
+	rpcCall(":4043", "Scheduler.HandleEvent", e)
+}
+
+func dispatch(e *event.Event) {
+	denormalize(e)
+	schedule(e)
+	log.Printf("Dispatched event: %v", e.Name)
+}
+
+//
+// HANDLERS
+//
+
+func setWallet(d event.Data) {
+	event := &event.Event{"walletWasSet", d}
+	dispatch(event)
+}
+
+
+func setUpkeep(d event.Data) {
+	event := &event.Event{"upkeepWasSet", d}
+	dispatch(event)
+}
+
+func setBalance(d event.Data) {
+	event := &event.Event{"balanceWasSet", d}
+	dispatch(event)
+}
+
+func setIncome(d event.Data) {
+	event := &event.Event{"incomeWasSet", d}
+	dispatch(event)
 }
