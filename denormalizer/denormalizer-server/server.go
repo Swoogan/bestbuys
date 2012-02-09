1deb661a0c2c819729c396dd230a7844954f9a0e
Colin Svingen <Colin Svingen@localhost>
2012-02-09 14:14:07 -0600

    Connected the denormalizer to mongodb

--- denormalizer/denormalizer-server/server.go
+++ denormalizer/denormalizer-server/server.go
@@ -6,16 +6,31 @@ import (
 	"net"
 	"log"
 	"http"
+	"launchpad.net/mgo"
 )
 
 func main() {
-	denorm := new(denormalizer.Denormalizer)
+	log.Printf("Connecting to mongodb")
+
+        session, err := mgo.Mongo("localhost")
+        if err != nil {
+                log.Fatal(err)
+                return
+        }
+        defer session.Close()
+
+        db := session.DB("uken")
+
+	denorm := denormalizer.New(db)
+
 	rpc.Register(denorm)
 	rpc.HandleHTTP()
 	log.Println("About to listen on localhost:4042")
-	l, e := net.Listen("tcp", ":4042")
-	if e != nil {
-		log.Fatal("listen error:", e)
+
+	l, err := net.Listen("tcp", ":4042")
+	if err != nil {
+		log.Fatal("listen error:", err)
 	}
+
 	http.Serve(l, nil)
 }
--- denormalizer/denormalizer.go
+++ denormalizer/denormalizer.go
@@ -4,11 +4,65 @@ import (
 	"os"
 	"log"
 	"eventbus/event"
+	"launchpad.net/mgo"
+	"launchpad.net/gobson/bson"
 )
 
-type Denormalizer struct {}
+type handler func(mgo.Database, event.Data) (err os.Error)
+type handlerPool map[string] handler
+
+type Denormalizer struct {
+	database mgo.Database
+	pool handlerPool
+}
+
+func New(database mgo.Database) *Denormalizer {
+	return &Denormalizer{
+		database,
+		handlerPool {
+			"walletWasSet": walletWasSet,
+			"upkeepWasSet": upkeepWasSet,
+			"balanceWasSet": balanceWasSet,
+			"incomeWasSet": incomeWasSet,
+		},
+	}
+}
 
 func (d *Denormalizer) HandleEvent(e *event.Event, i *int) os.Error {
-	log.Printf("Handling Event: %v", e.Name)
+	if handler, ok := d.pool[e.Name]; ok {
+		return handler(d.database, e.Data)
+	}
+
+	log.Printf("No handler specified for event: %v", e.Name)
+	return nil
+}
+
+// 
+// Handlers
+//
+
+func walletWasSet(database mgo.Database, data event.Data) (err os.Error) {
+	log.Println("Handling Event: walletWasSet")
+	selector := bson.M {"_id": bson.ObjectIdHex("4f18697b3946caf4198dd697")}
+	change := bson.M {"$set": bson.M{"wallet": data["wallet"]}}
+        if err = database.C("games").Update(selector, change); err != nil {
+                log.Println("Could not update the datastore")
+                log.Println(err)
+        }
+	return
+}
+
+func upkeepWasSet(database mgo.Database, data event.Data) os.Error {
+	log.Println("Handling Event: upkeepWasSet")
+	return nil
+}
+
+func balanceWasSet(database mgo.Database, data event.Data) os.Error {
+	log.Println("Handling Event: balanceWasSet")
+	return nil
+}
+
+func incomeWasSet(database mgo.Database, data event.Data) os.Error {
+	log.Println("Handling Event: incomeWasSet")
 	return nil
 }
