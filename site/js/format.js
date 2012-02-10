8d1e3ef56b5a9427651f7368151985d31cab95d0
Colin Svingen <Colin Svingen@localhost>
2012-02-10 11:59:29 -0600

    Implemented the tabs. Not sure if I like the styling, but they work

--- site/js/format.js
+++ site/js/format.js
@@ -15,9 +15,9 @@ function currencyFormat(amount)
 
 function parseCurrency(amount)
 {
-  if (amount.length === "") { return 0; }
+  if (amount === "") { return 0; }
   if (amount.charAt(0) == "$") { amount = amount.slice(1); }  
-  amount = amount.replace(",", "");
+  amount = amount.replace(/,/g, "");
   var i = parseInt(amount);
   if(isNaN(i)) { return "0"; }
   return i;
