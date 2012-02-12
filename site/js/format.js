5c6f4ac660634460ffe8bd4b91ed254a38f668a9
Colin Svingen <Colin Svingen@localhost>
2012-02-12 12:32:05 -0600

    Fixed bug where currency parsing wasn't handling the <br> that is add when enter is hit

--- site/js/format.js
+++ site/js/format.js
@@ -17,6 +17,7 @@ function parseCurrency(amount)
 {
   if (amount === "") { return 0; }
   if (amount.charAt(0) == "$") { amount = amount.slice(1); }  
+  amount = amount.replace(/<br>/g, "");
   amount = amount.replace(/,/g, "");
   var i = parseInt(amount);
   if(isNaN(i)) { return "0"; }
