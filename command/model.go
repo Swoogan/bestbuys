0134f0949f03125676b7d68d3cdfc7bc29d8f106
Colin Svingen <1377725+Swoogan@users.noreply.github.com>
2012-02-12 23:04:36 -0600

    Can now load and snapshot the model

--- command/model.go
+++ command/model.go
@@ -1,12 +1,46 @@
 package main
 
+import (
+	"os"
+	"log"
+	"launchpad.net/mgo"
+	"launchpad.net/gobson/bson"
+)
+
 type repository map[string]game
 
 func newRepository() repository {
 	return make(map[string]game, 3)
 }
 
+func (r repository) rebuild(d mgo.Database) {
+	var result []game
+	err := d.C("snapshot").Find(nil).All(&result)
+	if err != nil {
+		log.Println("Could not load snapshots:", err)
+		return
+	}
+
+	for _, game := range result {
+		r[game.id.Hex()] = game
+	}
+}
+
+func (r repository) snapshot(d mgo.Database) os.Error {
+	log.Println("in snapshot")
+	for id, game := range r {
+		selector := bson.M{"_id": id}
+		_, err := d.C("snapshot").Upsert(selector, game)
+		if err != nil {
+			log.Println("Could not save snapshot:", err)
+			return err
+		}
+	}
+	return nil
+}
+
 type game struct {
+	id bson.ObjectId `bson: "_id"`
 	finance finance
 	monies  monies
 }
