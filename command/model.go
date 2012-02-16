09449e2c9fa242b6512a9d5df540c22a930f5c9c
Colin Svingen <Colin Svingen@localhost>
2012-02-16 14:56:19 -0600

    Implemented snapshot timestamping:

--- command/model.go
+++ command/model.go
@@ -30,6 +30,7 @@ func (r repository) rebuild(d mgo.Database) {
 func (r repository) snapshot(d mgo.Database) os.Error {
 	log.Println("Saving snapshots")
 	for id, game := range r {
+		game.LastSaved = bson.Now()	// what if save fails???
 		selector := bson.M{"_id": bson.ObjectIdHex(id)}
 		_, err := d.C("snapshots").Upsert(selector, bson.M{"$set": game})
 		if err != nil {
