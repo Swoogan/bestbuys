30f54188d3111f31d33abc11b018f3c668315a90
Colin Svingen <1377725+Swoogan@users.noreply.github.com>
2012-02-22 15:54:05 -0600

    Updated the init script to have the lands

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
-!/bin/sh
+#!/bin/sh
 
-curl -i -X POST -d '{"name": "createGame", "data": {"name":"Age of Legends"}}' http://localhost/tasks/  
-curl -i -X POST -d '{"name": "createGame", "data": {"name":"Forces of War"}}' http://localhost/tasks/  
-curl -i -X POST -d '{"name": "createGame", "data": {"name":"Dark Galaxy"}}' http://localhost/tasks/  
+curl -i -H "Content-type: application/json" -X POST -d '{"name": "createGame", "data": {"name":"Age of Legends","lands":[{"name":"Rocky Coast","cost":50000,"income":500},{"name":"Fertile Valley","cost":500000,"income":3000},{"name":"Rustic Forest","cost":10000000,"income":20000},{"name":"Ancient Highlands","cost":100000000,"income":50000}]}}' http://localhost/tasks/  
+
+curl -i -H "Content-type: application/json" -X POST -d '{"name": "createGame", "data": {"name":"Forces of War","lands":[{"name":"Small Complex","cost":50000,"income":500},{"name":"Industrial Field","cost":500000,"income":3000},{"name":"Ocean Platform","cost":10000000,"income":20000},{"name":"Small Island","cost":100000000,"income":50000}]}}' http://localhost/tasks/  
+
+curl -i -H "Content-type: application/json" -X POST -d '{"name": "createGame", "data": {"name":"Dark Galaxy", "lands":[{"name":"Scorched Sands","cost":50000,"income":500},{"name":"Blistering Desert","cost":500000,"income":3000}]}}' http://localhost/tasks/  
