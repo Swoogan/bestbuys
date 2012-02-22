99e7cefba75450e27c28d1506e57987666d9aa4d
Colin Svingen <1377725+Swoogan@users.noreply.github.com>
2012-02-22 15:36:49 -0600

    Adding lands to the model

--- command/model.go
+++ command/model.go
@@ -1,50 +1,14 @@
 package main
 
 import (
-	"os"
-	"log"
-	"launchpad.net/mgo"
 	"launchpad.net/gobson/bson"
 )
-
-type repository map[string]game
-
-func newRepository() repository {
-	return make(map[string]game, 3)
-}
-
-func (r repository) rebuild(d mgo.Database) {
-	log.Println("Loading snapshots")
-	var result []game
-	err := d.C("snapshots").Find(nil).All(&result)
-	if err != nil {
-		log.Println("Could not load snapshots:", err)
-		return
-	}
-
-	for _, game := range result {
-		r[game.Id.Hex()] = game
-	}
-}
-
-func (r repository) snapshot(d mgo.Database) os.Error {
-	log.Println("Saving snapshots")
-	for id, game := range r {
-		game.LastSaved = bson.Now()	// what if save fails???
-		selector := bson.M{"_id": bson.ObjectIdHex(id)}
-		_, err := d.C("snapshots").Upsert(selector, bson.M{"$set": game})
-		if err != nil {
-			log.Println("Could not save snapshot:", err)
-			return err
-		}
-	}
-	return nil
-}
 
 type game struct {
 	Id        bson.ObjectId "hack" // this should be "_id" but then Upsert doesn't do anything
 	Finance   finance
 	Monies    monies
+	Lands []land
 	LastSaved bson.Timestamp
 }
 
@@ -69,4 +33,10 @@ type monies struct {
 
 func (m monies) total() int64 {
 	return m.Balance + m.Wallet + m.Lands
+}
+
+type land struct {
+	Name string
+	Cost int64
+	Income int64
 }
