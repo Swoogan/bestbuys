be74ab6c4a435264b9611b595f5f2ff1c8bc438e
Colin Svingen <1377725+Swoogan@users.noreply.github.com>
2012-03-05 20:18:57 -0600

    Getting rid of logic, everything has to go into the domain

--- domain/types.go
+++ domain/types.go
@@ -11,3 +11,27 @@ type Event struct {
 	Date bson.Timestamp
 	Data Data
 }
+
+type Result struct {
+	path  string
+	ratio uint64
+}
+
+type PurchaseResult struct {
+    Name string
+    Hours int
+    Quantity int
+    Cost domain.Money
+    Ratio domain.Money
+    Finance domain.Finance
+    IncomeIncrease domain.Money
+}
+
+type FullPurchase struct {
+    First PurchaseResult
+    Second PurchaseResult
+    IncomeIncrease domain.Money
+    Quantity int
+    Finance domain.Finance
+    TotalHours int
+}
