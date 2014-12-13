719ff50c489af4ac8bd3686d1291adbc411b31c8
Colin Svingen <1377725+Swoogan@users.noreply.github.com>
2014-12-13 16:27:54 -0500

    Have showing and hiding of message element

--- ngsite/app/game/game.js
+++ ngsite/app/game/game.js
@@ -12,15 +12,23 @@ angular.module('bestbuys.game', ['ngRoute'])
 .controller('GameCtrl', ['$scope', '$http', function($scope, $http) {
   //$http.get('/games/54177f9ef047050f92000004').
   $http.get('/games/54177f9ef047050f9200000').then(
-    function(result) {
+    function(result) {      
       $scope.finance = result.data;
     },
+    
     function(error) {
       console.log(error.statusText);
-      if (error.status == 404)
-	$scope.error = "The requested game data could not be found";
+      $scope.error = 'Error in: ' + error.config.url;
+      
+      if (error.status == 404)	
+	$scope.exception = "The requested game data could not be found.";
+      else if (error.status == 502)
+	$scope.exception = "The game data service appears unavailable. Try again later.";
       else
-	$scope.error = error.statusText;
+	$scope.exception = error.statusText;
+      
+      $scope.showError = true;
+
       /*
       $(this).removeClass('success');
       $(this).addClass('error');
