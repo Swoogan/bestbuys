1745025510e21d9fd89e5c23cd79d1fd8ce19cf7
Colin Svingen <1377725+Swoogan@users.noreply.github.com>
2015-02-05 22:13:19 -0500

    Moved to using service for GET

--- ngsite/app/game/services.js
+++ ngsite/app/game/services.js
@@ -4,7 +4,7 @@ var gameServices = angular.module('bestbuys.services', ['ngResource']);
 
 gameServices.factory('Game', ['$resource',
 	 function($resource){
-		 return $resource('games/:id', {}, {
-			 query: {method:'GET', params:{id:'54d42204f047050fc600000a'}}
+		 return $resource('/games/:id', {}, {
+			 query: {method:'GET', params:{id:'@id'}}
 		 });
 	 }]);
