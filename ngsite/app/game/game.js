a0aa0cc092b1427f1215499b763e811103c6ad20
Colin Svingen <swoogan@gmail.com>
2015-02-16 21:40:36 -0500

    Finished conversion to Angular services

--- ngsite/app/game/game.js
+++ ngsite/app/game/game.js
@@ -9,16 +9,16 @@ angular.module('bestbuys.game', ['ngRoute'])
   });
 }])
 
-.controller('GameCtrl', ['$scope', '$routeParams', 'Game', 'Command', 'ErrorMessage',
-  function($scope, $routeParams, Game, Command, ErrorMessage) {
-    $scope.gameId = '54d42204f047050fc600000a';
+.controller('GameCtrl', ['$scope', '$routeParams', 'Game', 'Command', 'Notification',
+  function($scope, $routeParams, Game, Command, Notification) {
+    $scope.gameId = '54d42204f047050fc600000a';    
     
     Game.get({id: $scope.gameId},
       function(data) {
-	$scope.finance = data;
+        $scope.finance = data;
       },
-      function(error) {
-	$scope.message = ErrorMessage.generate(error);      
+      function (error) {
+        $scope.message = Notification.showError(error);
       }
     );
     
@@ -27,14 +27,29 @@ angular.module('bestbuys.game', ['ngRoute'])
       var data = { game: $scope.gameId };
       data[field] = value;
       
-      Command.save(command, data);
+      Command.save(command, data).then(
+        function () {
+          $scope.message = Notification.showSuccess();
+        },
+        function (error) {
+          $scope.message = Notification.showError(error);
+        }
+      );
     }
     
     $scope.saveStructure = function(name, value) {        
       var data = { structureCost: value, structureName: name, game: $scope.gameId };
-      Command.save('setStructureCost', data);
+      Command.save('setStructureCost', data).then(
+        function () {
+          $scope.message = Notification.showSuccess();
+        },
+        function (error) {
+          $scope.message = Notification.showError(error);
+        }
+      );
     }    
   }
 ]); 
 
 
+	  
