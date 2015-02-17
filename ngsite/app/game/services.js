0c8457606cce5fd954418451673b94582bc3d9dc
Colin Svingen <1377725+Swoogan@users.noreply.github.com>
2015-02-16 21:40:36 -0500

    Finished conversion to Angular services

--- ngsite/app/game/services.js
+++ ngsite/app/game/services.js
@@ -2,47 +2,39 @@
 
 var gameServices = angular.module('bestbuys.services', ['ngResource']);
 
-gameServices.factory('Game', ['$resource',
-		     function($resource) {
-		       return $resource('/games/:id', {id:'@id'});		       
-		    }]);
+gameServices.factory('Game', ['$resource', function($resource) {
+    return $resource('/games/:id', {id:'@id'});
+}]);
 
-gameServices.factory('ErrorMessage', [function() {
-  return {
-    generate: function(error) {
-      var message = {
-	error: true,
-	show: true,
-	text: 'Error in: ' + error.config.url
-      };
-      
-      if (error.status == 404)
-	message.details = "The requested game data could not be found.";
-      else if (error.status == 502)
-	message.details = "The game data service appears unavailable. Try again later.";
-      else
-	message.details = error.statusText;
-      
-      return message;
-    }
+gameServices.service('Notification', [function() {  
+  this.showSuccess = function () {	      
+    return {
+      error: false,
+      show: true,
+      text: 'Successfully saved changes.'
+    };      
+  };
+  
+  this.showError = function(error) {
+    var message = {
+      error: true,
+      show: true,
+      text: 'Error in: ' + error.config.url
+    };
+    
+    if (error.status == 404)
+      message.details = "The requested game data could not be found.";
+    else if (error.status == 502)
+      message.details = "The game data service appears unavailable. Try again later.";
+    else
+      message.details = error.statusText;
+    
+    return message;
   };
 }]);
   
-gameServices.factory('Command', ['$http', function($http) { 
-  return {
-    save: function (name, data, message) {	    
-	    $http.post('/commands/', {name: name, data: data}).then(      
-	      function () {	
-		message = {
-		  error: false,
-		  show: true,
-		  text: 'Successfully saved changes.'
-		};
-	      },
-	      function(error) {	
-		message = buildError(error);
-	      }
-	    );	    
-	  }
-  };
+gameServices.service('Command', ['$http', function($http) {   
+  this.save = function (name, data) {
+    return $http.post('/commands/', {name: name, data: data});
+  };  
 }]);
