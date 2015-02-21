7a83b8cb1618ab737bf9e551217b1e9e18e675ea
Colin Svingen <swoogan@gmail.com>
2015-02-21 11:46:25 -0500

    Updated todo

--- ngsite/app/purchases/services.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var services = angular.module('bestbuys.purchasesServices', ['ngResource']);
-
-services.service('Command', ['$http', function($http) {   
-  this.save = function (name, data) {
-    return $http.post('/commands/', {name: name, data: data});
-  };  
-}]);
